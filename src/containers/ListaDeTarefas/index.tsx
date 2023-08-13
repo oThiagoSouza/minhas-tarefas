1c5c93474a4e0c1cf5da3c053c9d8bce283d4004
oThiagoSouza <souzaalvesthiago99@gmail.com>
2023-08-13 05:19:21 -0300

    Continuando parametrização componentes

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,25 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'Estudar TS',
+    descricao: 'Ver a aula sem chorar',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'Fazer atividade faculdade',
+    descricao: 'Terminar as AO01',
+    prioridade: 'urgente',
+    status: 'concluída'
+  },
+  {
+    titulo: 'Pagar contas',
+    descricao: 'Pagar cartões de crédito',
+    prioridade: 'normal',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
     <p>2 tarefas marcadas como &quot;categoria&ldquo; e &quot;termo&ldquo;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            prioridade={t.prioridade}
+            status={t.status}
+            titulo={t.titulo}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
